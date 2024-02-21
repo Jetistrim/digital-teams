4132576deb85e0998852b36ba8d0c1f681bcdb61
Paulo Gabriel Lima de Sousa <paulogabriellima@hotmail.com>
2024-02-20 23:43:15 -0300

    bug do ShowHide resolvido

--- js/index.js
+++ js/index.js
@@ -15,6 +15,7 @@
 // }
 
 let teams = []
+let teamsArr = []
 
 function toggleShow(addORrem) {
     const action = addORrem ? 'add' : 'remove';
@@ -30,30 +31,15 @@ const closeActions = () => toggleShow(false);
 closeForm.onclick = closeActions;
 overlay.onclick = closeActions;
 
-formCriar.onsubmit = () => {
+formCriar.onsubmit = () => { // Ao enviar, ocorre isso
     event.preventDefault();
-    teams.push({
+    teams.push({ // insere na array 'teams' as informações
         name: nome.value,
         capacity: capacidade.value,
         members: []
     })
 
-    adicionarCards()
-
-    function changeBoxIcon(icon){
-        if (icon.getAttribute('name') === 'show'){
-            icon.setAttribute('name', 'hide');
-        } else {
-            icon.setAttribute('name', 'show');
-        };
-    };
-
-    ShowHide.onclick[changeBoxIcon()]
-
-    const icon = document.getElementById('ShowHide');
-    icon.addEventListener('click', function() {
-        changeBoxIcon(this);
-    });
+    adicionarCards() // aciona a function
 
     overlay.classList.remove('show');
     formCriar.classList.remove('show');
@@ -61,17 +47,36 @@ formCriar.onsubmit = () => {
 
 
 function adicionarCards(){
+    let teamsAdded = {}
     listTeams.innerHTML = ' ';
     for (let i = 0; i < teams.length; i++){
-        listTeams.innerHTML += `
-                        <li>
-                            <h4> ${teams[i].name} <box-icon name='show' id='ShowHide'></box-icon></h4>
-                            <h1>0 <span>/ ${teams[i].capacity}</span></h1>
-                            <div class="actions">
-                                <button>adicionar</button>
-                                <button><box-icon name='trash'></box-icon></button>
-                            </div>
-                        </li>
-                        `;
+        if (!teamsAdded[teams[i].name]){
+            let cardHTML = `
+            <li>
+                <h4> ${teams[i].name} <box-icon name='show' id='ShowHide${i}' onclick="changeBoxIcon(${i})"></box-icon></h4>
+                <h1>0 <span>/ ${teams[i].capacity}</span></h1>
+                <div class="actions">
+                    <button>adicionar</button>
+                    <button><box-icon name='trash'></box-icon></button>
+                </div>
+            </li>
+            `;
+            teamsArr.push(cardHTML);
+            teamsAdded[teams[i].name] = true
+            listTeams.innerHTML += cardHTML
+            console.log(teamsAdded);
+        }
+        // CRIAR TELA "NOME DE TEAM JÁ UTILIZADO"
+        // FAZER O DELETE DA li
+        // FAZER O "hide" DAS INFORMAÇÕES
     }
 }
+
+function changeBoxIcon(b){ //função para mudar o icone do olho
+    let icon = document.getElementById('ShowHide'+b)
+    if (icon.getAttribute('name') === 'show'){ 
+        icon.setAttribute('name', 'hide');
+    } else {
+        icon.setAttribute('name', 'show');
+    };
+};
